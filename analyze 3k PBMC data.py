72e85551f91af19ef804799e67a6e87afc860814
Chendd <chendd3038@gmail.com>
2022-01-30 11:23:51 +0800

    Initial commit

--- analyze 3k PBMC data.py
+++ analyze 3k PBMC data.py
@@ -7,7 +7,9 @@ import seaborn as sns
 sc.settings.verbosity = 3             # verbosity: errors (0), warnings (1), info (2), hints (3)
 sc.logging.print_header()
 sc.settings.set_figure_params(dpi=80, facecolor='white')
-results_file = '/Users/chendd/write/pbmc3k.h5ad'  # the file that will store the analysis results
+
+
+results_file = '/Users/chendd/data/write/pbmc3k.h5ad'  # the file that will store the analysis results
 adata = sc.read_10x_mtx(
     '/Users/chendd/data/filtered_gene_bc_matrices/hg19/',  # the directory with the `.mtx` file and '.tsv' file. # where to get these formated files?
     var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
@@ -45,8 +47,41 @@ sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False,
 # sc.pl.scatter(adata, x='total_counts', y='pct_counts_mt')
 # sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts')
 
-print(adata[adata.obs.n_genes_by_counts < 2500, :])
 
-# adata = adata[adata.obs.n_genes_by_counts < 2500, :]
-# adata = adata[adata.obs.pct_counts_mt < 5, :]
+##mainly, scripts below filter cells based on n_gens and pct_count_mt.
+adata = adata[adata.obs.n_genes_by_counts < 2500, :]
+adata = adata[adata.obs.pct_counts_mt < 5, :]
+sc.pp.normalize_total(adata, target_sum=1e4)  #Total-count normalize (library-size correct) the data matrix  to 10,000 reads per cell, so that counts become comparable among cells.
+sc.pp.log1p(adata)  #Logarithmize the data
+
+sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)  # Identify highly-variable genes.
+# sc.pl.highly_variable_genes(adata)
+adata.raw = adata  # Set the .raw attribute of the AnnData object to the normalized and logarithmized raw gene expression for later use in differential testing and visualizations of gene expression. This simply freezes the state of the AnnData object.
+
+adata = adata[:, adata.var.highly_variable]
+sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt']) #Regress out (mostly) unwanted sources of variation.
+sc.pp.scale(adata, max_value=10)
+sc.tl.pca(adata, svd_solver='arpack')
+sc.pl.pca(adata, color='CST3')
+sc.pl.pca_variance_ratio(adata, log=True)
+adata.write(results_file)
+
+print(adata)
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
+sc.tl.umap(adata)
+sc.pl.umap(adata, color=['CST3', 'NKG7', 'PPBP'])
+sc.pl.umap(adata, color=['CST3', 'NKG7', 'PPBP'], use_raw=False)
+sc.pl.umap(adata, color=['leiden', 'CST3', 'NKG7'])
+adata.write(results_file)
+
+
+
+# print(adata)
+# print(adata.obs)
+# print(adata.var)
+
+
+
+
+
 
